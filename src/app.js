31e85cf1c71c042e4fa4aec82c7fc3e17221c3a3
C137Adrian <adriancanoespinosa@gmail.com>
2025-04-02 18:22:12 +0000

    Primer commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-import "bootstrap";
-import "./style.css";
 
+const phrase = ()=>{
+  let who =['The dog','My grandma','The mailman','My bird'];
+  let action = ['ate','peed','crushed','broke'];
+  let what = ['my homework','my phone','the car','the computer'];
+  let when = ['before the class','when I was sleeping','while I was exercising','during my lunch','while I was praying'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  return `${who[Math.floor(Math.random()*who.length)]}
+  ${action[Math.floor(Math.random()*action.length)]}
+  ${what[Math.floor(Math.random()*what.length)]}
+  ${when[Math.floor(Math.random()*when.length)]}`;
+  
+}
+document.getElementById("excuse").textContent = phrase();
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const generatorColor =()=>{
+  const gnCol =['col-red','col-blue','col-yellow'];
+
+  return gnCol[Math.floor(Math.random()*gnCol.length)];
+}
+document.body.classList.add(generatorColor());
